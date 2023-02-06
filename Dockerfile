c6085db8ef6f73f92129de1d7230d71c24d4fd12
devketanpro <devketanpro11@gmail.com>
2023-02-06 18:58:36 +0530

    add env and requirements.txt file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,13 @@
 FROM python:3.6
+# install python requirements
+RUN pip install --upgrade pip
+RUN pip install pip-tools  # for building new requirements.txt files
+
+# copy just requirements and install before rest of code to avoid having to
+# reinstall packages during build every time code changes
+COPY requirements.txt requirements.txt
+COPY .env .env
+RUN pip install -r requirements.txt
 LABEL  Maintainer="devketanpro@me17"
 WORKDIR /app
 COPY generate_token.py ./
--- generate_token.py
+++ generate_token.py
@@ -1,18 +1,27 @@
-import urllib.request
+import os
+from dotenv.main import load_dotenv
 import socketserver
 import http.server
+import jwt
+import http.client
 
-PORT = 9098
+env = load_dotenv()  # load the env file
+HTTP_PORT = 9098  # access port for do any post request
+KEY = os.environ['PRIVATE_KEY']  # key to encode/decode token.
 
 
-class MyProxy(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        url = self.path[1:]
-        self.send_response(200)
+class MyProxy(http.server.CGIHTTPRequestHandler):
+    def do_POST(self):
+        # for payload
+        req_user = self.headers['date']  # timestamp
+        req_date = self.headers['user']  # username
+        encoded = jwt.encode(
+            {os.environ['USERNAME_KEY']: req_user, os.environ['DATE_KEY']: req_date}, KEY, algorithm=os.environ['ALGORITH'])
+        self.send_response(http.client.OK)
         self.end_headers()
-        self.copyfile(urllib.request.urlopen(url), self.wfile)
+        print(f"JWT-token: {encoded}")
 
 
-httpd = socketserver.ForkingTCPServer(('', PORT), MyProxy)
-print("Now serving at ", str(PORT))
+httpd = socketserver.ForkingTCPServer(('', HTTP_PORT), MyProxy)
+print("Available port for making post request:- ", str(HTTP_PORT))
 httpd.serve_forever()
